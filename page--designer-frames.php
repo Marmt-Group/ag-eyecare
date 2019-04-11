83a31129da0c0fb04f5dfb051d5462617b4fbbcf
David Davis <davidjamesdavis.djd@gmail.com>
2019-04-10 18:43:39 -0700

    pushing designer frames newer version

--- page--designer-frames.php
+++ page--designer-frames.php
@@ -15,7 +15,7 @@ get_header();
 <?php get_template_part( '/template-parts/content', 'nav' ); ?>
 
 <div class="main-container">
-    <section class="section-snippet-about-2 bg--secondary container-header imagebg">
+    <section class="section-snippet-about-2 bg--secondary container-header imagebg" style="background-image: url(<?php the_field('header_image'); ?>);">
         <div class="container">
             <div class="row pos-vertical-align-columns">
                 <div class="col-sm-5">
@@ -47,250 +47,51 @@ get_header();
         </div>
     </section>
 
+    <?php if( have_rows('brands') ): ?>
     <section class="section-snippet-about-2 bg--secondary container-logos">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center">
                     <h3 style="margin:1.5em 0 0;font-size:2em;">Top brands include:</h3>
                     <div class="partner-logos">
-                        <img alt="Coach prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/Coach_New_Logo.svg">
-                        <img alt="Gucci prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/Gucci_logo.svg">
-                        <img alt="Lacoste prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/lacoste-1-logo-svg-vector.svg">
-                        <img alt="Ray Ban prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/ray-ban-logo-svg-vector.svg">
-                        <img alt="Tiffany & Co prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/Tiffany_Logo.svg">
-                        <img alt="Persol prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/Persol_logo.svg">
-                        <img alt="Prada prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Prada-Logo.svg">
-                        <img alt="Dolce and Gabbana prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Dolce_and_Gabbana.svg">
-                        <img alt="Versace prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Versace_logo.svg">
-                        <img alt="Burberry prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Burberry_Logo.svg">
-                        <img alt="Bulgari prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Bulgari_logo.svg">
-                        <img alt="Fendi prescription & sunglasses" src="<?php echo get_template_directory_uri()  ?>/img/Fendi_logo.svg">
+                      <?php while ( have_rows('brands') ) : the_row(); 
+                        $logo = get_sub_field('logo');
+                      ?>
+                        <img alt="" src="<?php echo $logo; ?>">
+                      <?php endwhile; ?>
                     </div>
                     <h3 style="margin:1.5em 0 0;font-size:1em;">and many more</h3>
                 </div>
             </div>
         </div>
     </section>
+    <?php endif; ?>
 
-    <section class="section-hero-3 section--card container-slider">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="slider box-shadow" data-animation="slide" data-arrows="true" data-timing="5000">
-                        <ul class="slides">
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Designer Frames Trend Book image" src="<?php echo get_template_directory_uri() ?>/img/products_carousel.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Unmatched luxury from leading designers image" src="<?php echo get_template_directory_uri() ?>/img/products_carousel2.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Take on anything and stay on-trend image" src="<?php echo get_template_directory_uri() ?>/img/products_carousel3.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Dolce Y Gabbana prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel4.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Kate Spade prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel5.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Gucci prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel6.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Diane Von Furstenburg prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel7.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Marc Jacobs prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel8.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Miu Miu prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel9.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Etnia Barcelona prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel10.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Etnia Barcelona prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel11.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Tom Ford prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel12.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Tom Ford prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel13.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Coach prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel14.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Versace prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel15.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Ray Ban prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel16.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Ray Ban prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel17.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Skaga prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel18.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Michael Kors prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel19.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Persol prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel20.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Persol prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel21.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Persol prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel22.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Persol prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel23.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Fendi prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel24.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Burberry prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel25.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Tiffany & Co prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel26.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Polo prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel27.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Valentino prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel28.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Prada prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel29.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Prada prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel30.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Eco prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel31.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Eco prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel32.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Chloe prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel33.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Tory Burch prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel34.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Bvlgari prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel35.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Lacoste prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel36.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Fun styles for little ones prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel37.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Ray Ban prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel38.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Nike prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel39.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Lacoste prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel40.jpg">
-                                </div>
-                            </li>
-                            <li class="imagebg" >
-                                <div class="background-image-holder">
-                                    <img alt="Disney prescription & sunglasses" src="<?php echo get_template_directory_uri() ?>/img/products_carousel41.jpg">
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php if( have_rows('slides') ): ?>
+      <section class="section-hero-3 section--card container-slider add-glasses-2">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12">
+                      <div class="slider box-shadow" data-animation="slide" data-arrows="true" data-timing="5000">
+                          <ul class="slides">
+                            <?php while ( have_rows('slides') ) : the_row(); 
+                              $image = get_sub_field('image');
+                            ?>
+                              <li class="imagebg" >
+                                  <div class="background-image-holder">
+                                      <img alt="<?php echo $image['alt']; ?>" src="<?php echo $image['url']; ?>">
+                                  </div>
+                              </li>
+                            <?php endwhile; ?>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+    <?php endif; ?>
 
-    <section class="container-summary bg--primary add-glasses-3">
+    <section class="container-summary bg--primary">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12 text-center">
